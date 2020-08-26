2ded1523ea3ee9799806cd7121d744bc65cc1506
EricksonLopes <ofc.erickson@gmail.com>
2020-08-26 12:32:32 -0300

    Implementando GET Hotel

--- requests_teste.py
+++ requests_teste.py
@@ -1,10 +1,27 @@
 import requests
 
-# get
-get = requests.get("http://127.0.0.1:5000/hoteis")
 
-hoteis = get.json()['hoteis']
+def get_hoteis():
+    # get
+    get = requests.get("http://127.0.0.1:5000/hoteis")
 
-for hotel in hoteis:
-    print(hotel)
+    hoteis = get.json()['hoteis']
 
+    for hotel in hoteis:
+        print(hotel)
+
+
+def get_hotel(pk):
+    get = requests.get("http://127.0.0.1:5000/hoteis/" + str(pk))
+    print(get.json())
+
+
+def put_hotel(pk):
+    put = requests.put("http://127.0.0.1:5000/hoteis/" + str(pk))
+
+
+def delete_hotel(pk):
+    delete = requests.delete("http://127.0.0.1:5000/hoteis/" + str(pk))
+
+
+get_hotel(1)
--- resources/hotel.py
+++ resources/hotel.py
@@ -2,21 +2,21 @@ from flask_restful import Resource
 
 hoteis = [
     {
-        'hotel_id': '1',
+        'hotel_id': 1,
         'estrelas': '4',
         'nome': 'homelandia',
         'diaria': 400.00,
         'cidade': 'São Paulo'
     },
     {
-        'hotel_id': '2',
+        'hotel_id': 2,
         'estrelas': '5',
         'nome': 'bravo',
         'diaria': 500.00,
         'cidade': 'Minas Gerais'
     },
     {
-        'hotel_id': '3',
+        'hotel_id': 3,
         'estrelas': '3',
         'nome': 'H4',
         'diaria': 300.00,
@@ -28,3 +28,20 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'hotel not found'}, 404
+
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
